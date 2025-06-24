9862b80148a0b020060cc977892e7723e3bd12c3
Luigi Minardi <lugminsinlag@gmail.com>
2025-06-24 14:05:49 +0200

    feat: Adding healthz endpoint

--- main.go
+++ main.go
@@ -11,8 +11,17 @@ func main() {
 	srv.Handler = mux
 	srv.Addr = ":8080"
 
-	mux.Handle("/assets/", http.FileServer(http.Dir(".")))
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/assets/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		_, err := w.Write([]byte("OK"))
+		if err != nil {
+			log.Printf("helthz failed to write with error: %v", err)
+		}
+	})
 
 	log.Printf("HTTP server started on http://localhost%v\n", srv.Addr)
 	if err := srv.ListenAndServe(); err != nil {
